79049408b3e86f035ecf7d7013886508a5ca0866
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:43:08 +0300

    1_end

--- PythonApplication1/PythonApplication1.py
+++ PythonApplication1/PythonApplication1.py
@@ -1,1 +1,19 @@
 
+positive_count = 0
+negative_count = 0
+
+while True:
+ 
+    number = int(input("Введите число: "))
+    
+    if number == 0:
+        break
+    
+    if number > 0:
+        positive_count += 1
+    elif number < 0:
+        negative_count += 1
+
+print(f"Кол-во положительных чисел: {positive_count}")
+print(f"Кол-во отрицательных чисел: {negative_count}")
+
