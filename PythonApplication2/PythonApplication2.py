e116d92d49b8448ea6b53539fe7da20ca0316ce2
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:45:50 +0300

    2_end

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,1 +1,19 @@
 
+
+total_tasks = 0
+called_wife = False
+
+
+for hour in range(1, 9):
+    tasks = int(input(f"{hour}-й час\nСколько задач решит Максим? "))
+    total_tasks += tasks
+    
+    call = int(input("Звонит жена. Взять трубку? (1 — да, 0 — нет): "))
+    
+    if call == 1:
+        called_wife = True
+
+print(f"Максим выполнил {total_tasks} задач.")
+
+if called_wife:
+    print("Нужно зайти в магазин.")
