630d59d40015e99c1d5f0958fc6b3deac2d264ed
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:52:09 +0300

    4_end

--- PythonApplication4/PythonApplication4.py
+++ PythonApplication4/PythonApplication4.py
@@ -1,1 +1,13 @@
 
+
+total_salary = 0
+
+for month in range(1, 13):
+    salary = float(input(f"Введите зарплату за месяц {month}: "))
+ 
+    total_salary += salary
+
+
+average_salary = total_salary / 12
+
+print(f"Средняя зарплата за год: {average_salary:.2f}")
