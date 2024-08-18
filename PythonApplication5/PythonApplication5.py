bcb80bd9d763a21128d3cfb4b2bffc963e3d95d8
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:55:11 +0300

    5_end

--- PythonApplication5/PythonApplication5.py
+++ PythonApplication5/PythonApplication5.py
@@ -1,1 +1,19 @@
 
+
+N = int(input("Введите число карточек — N: "))
+
+
+total_sum = N * (N + 1) // 2
+
+
+remaining_sum = 0
+
+
+for _ in range(N - 1):
+    card_number = int(input("Введите номер оставшейся карточки: "))
+    remaining_sum += card_number
+
+
+lost_card = total_sum - remaining_sum
+
+print(f"Потерянная карточка: {lost_card}")
