67bdeadf05a56e043b3f87a55e4a0e78297ebe66
Uladzislau Balan <vladislav.balan96@gmail.com>
2024-08-18 20:49:35 +0300

    3try

--- PythonApplication3/PythonApplication3.py
+++ PythonApplication3/PythonApplication3.py
@@ -1,1 +1,19 @@
 
+
+secret_number = 7
+
+attempts = 0
+
+while True:
+ 
+    guess = int(input("Введите число: "))
+    
+    attempts += 1
+    
+    if guess < secret_number:
+        print("Число меньше, чем нужно. Попробуйте ещё раз!")
+    elif guess > secret_number:
+        print("Число больше, чем нужно. Попробуйте ещё раз!")
+    else:
+        print(f"Вы угадали! Число попыток: {attempts}")
+        break
